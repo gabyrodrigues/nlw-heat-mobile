e63e433af7bb2dba7b095c93b4b60923d566b8e9
gabyrodrigues <gabyrlima.99@gmail.com>
2022-04-11 22:29:36 -0300

    creating app header with static user image

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
+import AppLoading from 'expo-app-loading'
 
 import { Home } from './src/screens/Home'
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if(!fontsLoaded) { //shows splash screen until fonts are loaded
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
   );
 }

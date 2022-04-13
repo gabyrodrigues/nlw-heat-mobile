3c14635136985ac9bc66ee2c1ef38f12781304f5
gabyrodrigues <gabyrlima.99@gmail.com>
2022-04-13 20:26:47 -0300

    logout and list last 3 messages from backend

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
-import { Message } from '../Message'
+import { api } from '../../services/api'
+import { Message, MessageProps } from '../Message'
 
 import { styles } from './styles'
 
 export function MessageList() {
-  const message = {
-    id: '1',
-    text: 'Mensagem de teste',
-    user: {
-      name: 'Gaby Rodrigues',
-      avatar_url: 'https://github.com/gabyrodrigues.png'
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([])
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>('/messages/last3')
+      setCurrentMessages(messagesResponse.data)
     }
-  }
+
+    fetchMessages()
+  }, [])
 
   return (
     <ScrollView 
@@ -20,9 +23,9 @@ export function MessageList() {
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+      {currentMessages.map(message => (
+        <Message key={message.id} data={message} />
+      ))}
     </ScrollView>
   )
 }
